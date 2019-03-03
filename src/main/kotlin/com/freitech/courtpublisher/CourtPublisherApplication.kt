ce075695aa91fd74157b1292411cf11fb0352b5d
shimbuichi <contact.shimbuichi@gmail.com>
2019-03-04 07:04:50 +0900

    initial commit

---
+++ src/main/kotlin/com/freitech/courtpublisher/CourtPublisherApplication.kt
@@ -0,0 +1,11 @@
+package com.freitech.courtpublisher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CourtPublisherApplication
+
+fun main(args: Array<String>) {
+    runApplication<CourtPublisherApplication>(*args)
+}
